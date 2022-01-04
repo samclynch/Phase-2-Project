e97253c1c255cdac849dbdee4296f4f324e8311a
abdulrahman muse <abdulrahmanmuse1@gmail.com>
2022-01-04 22:58:45 +0300

    added a search bar, add color form, some react routers, and nav links

--- color-pallet-project/src/App.js
+++ color-pallet-project/src/App.js
@@ -1,9 +1,18 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import AddColorForm from './AddColorForm';
 import './App.css';
 import ColorPalleteItems from './ColorPalleteItems';
+import Search from './Search';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   const [colors, setColors] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:3000/colors")
@@ -14,10 +23,34 @@ function App() {
   }, []);
 
 
+  const addColor = (formData) => {
+    fetch("http://localhost:3000/colors", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((resp) => resp.json())
+      .then((newCr) => setColors([...colors, newCr]));
+  };
+
+
+
 
   return (
     <div className="App">
-      <ColorPalleteItems colors={colors}/>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <ColorPalleteItems colors={colors} searchTerm={searchTerm} />
+            <Search setSearchTerm={setSearchTerm} />
+          </Route>
+          <Route path="/add">
+            <AddColorForm addColor={addColor} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- color-pallet-project/src/ColorPalleteItems.js
+++ color-pallet-project/src/ColorPalleteItems.js
@@ -1,39 +1,47 @@
-import { useState, useEffect} from "react"
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { useState, useEffect } from "react"
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { NavLink } from "react-router-dom";
 
 
-function ColorPalleteItems({colors}){
+
+function ColorPalleteItems({ colors, searchTerm }) {
     const [background, setBackground] = useState("#071415");
     const [current, setCurrent] = useState(null);
+    const crToShow = colors.filter((cr) =>
+        cr.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-    useEffect(()=>{
-        const timeoutId= setTimeout(()=> {
-          setCurrent(null)
-        },3000)
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setCurrent(null)
+        }, 3000)
         return () => clearTimeout(timeoutId)
-      },[current])
+    }, [current])
 
     return (
-        <div className="App" style ={{background : background}}>
-        {current !== null && <h1> Copied {current}</h1>}
-        <div className="container">
-        {colors.map((color) => (
-          <div  key={color.name}className = "card">
-            <div style={{
-              background:color.name,
-              filter:"brightness(85%)",
-              boxShadow: color.name === background ? "0 0 5px #000" : ""}}
-              className="box" 
-              onClick={()=> setBackground(color.name)}>
+        <div className="App" style={{ background: background }}>
+            <NavLink className='button' exact to="/">Home</NavLink>
+            <NavLink className='button' exact to="/add">Add Color</NavLink>
+            {current !== null && <h1> Copied {current}</h1>}
+            <div className="container">
+                {crToShow.map((color) => (
+                    <div key={color.name} className="card">
+                        <div style={{
+                            background: color.name,
+                            filter: "brightness(85%)",
+                            boxShadow: color.name === background ? "0 0 5px #000" : ""
+                        }}
+                            className="box"
+                            onClick={() => setBackground(color.name)}>
+                        </div>
+                        <CopyToClipboard text={`color: ${color.name};`}>
+                            <p style={{ color: color.name === background ? "#fff" : color.name, padding: "7px" }}
+                                onClick={(() => setCurrent(color.name))}>{color.name}</p>
+                        </CopyToClipboard>
+                    </div>
+                ))}
             </div>
-            <CopyToClipboard text={`color: ${color.name};`}>
-              <p style={{color: color.name === background ? "#fff" : color.name, padding:"7px" }} 
-              onClick={(()=>setCurrent(color.name))}>{color.name}</p>
-            </CopyToClipboard>
-          </div>
-        ))}
-      </div>
-      </div>
+        </div>
     )
 }
 
